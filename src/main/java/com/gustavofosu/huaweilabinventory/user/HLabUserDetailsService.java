e4673fc3f194fdc4f837ebc197fce247efd3bab4
gustavCode <gustavofosu@gmail.como>
2022-08-02 08:52:22 +0000

    Adding comments to the service classes

--- src/main/java/com/gustavofosu/huaweilabinventory/user/HLabUserDetailsService.java
+++ src/main/java/com/gustavofosu/huaweilabinventory/user/HLabUserDetailsService.java
@@ -8,12 +8,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class HLabUserDetailsService implements UserDetailsService {
 
+    // private fields
     private final HLabUsersRepository hLabUsersRepository;
 
+    // constructor injection with HLabRepository as parameter
     public HLabUserDetailsService(HLabUsersRepository hLabUsersRepository) {
         this.hLabUsersRepository = hLabUsersRepository;
     }
 
+    // method to find and return user details by the username
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         HLabUsers hLabUser = hLabUsersRepository.findByUsername(username);
